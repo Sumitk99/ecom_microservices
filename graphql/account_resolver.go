06a927ce37245e1c698ac9acea6d46877ee4ab68
Sumitk99 <sumit2k19kumar@gmail.com>
2024-10-12 00:12:16 +0530

    Initial commit, graphql services are done, working on account microservice

---
+++ graphql/account_resolver.go
@@ -0,0 +1,9 @@
+package main
+
+type accountResolver struct {
+	server *Server
+}
+
+//func (r *accountResolver) Order(ctx context.Context, obj *Account) ([]*Order, error) {
+//	//return r.server.accountClient.GetAccounts(ctx, pagination, id)
+//}
---
+++ graphql/query_resolver.go
@@ -0,0 +1,13 @@
+package main
+
+type queryResolver struct {
+	server *Server
+}
+
+//func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string)([]*Account, error){
+//	return r.server.accountClient.GetAccounts(ctx, pagination, id)
+//}
+
+//func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput,query, id *string)([]*Product, error){
+//	return r.server.accountClient.GetAccounts(ctx, pagination, id)
+//}
